fc6958e964883e9a67a5902666300fea60a5a2a1
MohamedKha1ed <mohamed.khaled1110@gmail.com>
2017-02-09 13:12:15 +0200

    edit parent model

--- app/src/main/java/Models/Parent.java
+++ app/src/main/java/Models/Parent.java
@@ -7,7 +7,7 @@ import java.util.Date;
  * Created by mohamed on 2/9/17.
  */
 
-public class Parent extends Student{
+public class Parent extends User{
     private ArrayList<Student> children = new ArrayList<Student>();
 
     public Parent() {
@@ -15,8 +15,8 @@ public class Parent extends Student{
         this.children = new ArrayList<Student>();
     }
 
-    public Parent(String firstName, String middleName, String lastName, Date dateOfBirth, String gender, String email, Date createdAt, Date updatedAt, String avatar, String userType, String phone, String mobile, String level, String section, String stage, int bedoPoints, Parent parent, ArrayList<Student> children) {
-        super(firstName, middleName, lastName, dateOfBirth, gender, email, createdAt, updatedAt, avatar, userType, phone, mobile, level, section, stage, bedoPoints, parent);
+    public Parent(String firstName, String middleName, String lastName, Date dateOfBirth, String gender, String email, Date createdAt, Date updatedAt, String avatar, String userType, String phone, String mobile, ArrayList<Student> children) {
+        super(firstName, middleName, lastName, dateOfBirth, gender, email, createdAt, updatedAt, avatar, userType, phone, mobile);
         this.children = children;
     }
 
