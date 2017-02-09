87ec7a88f0a562905ba7db80552bec6e2b1239d1
Mohamed Khaled <mohamed.khaled1110@gmail.com>
2017-02-09 10:16:03 +0200

    build project structure (#1)

---
+++ app/src/main/java/Controllers/UserCtrl.java
@@ -0,0 +1,8 @@
+package Controllers;
+
+/**
+ * Created by mohamed on 2/9/17.
+ */
+
+public class UserCtrl {
+}
---
+++ app/src/main/java/Models/User.java
@@ -0,0 +1,8 @@
+package Models;
+
+/**
+ * Created by mohamed on 2/9/17.
+ */
+
+public class User {
+}
