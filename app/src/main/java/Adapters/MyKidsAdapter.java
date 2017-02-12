9411bea2990bf1c864388bfc119a5df8d6b3886f
Mohamed Khaled <mohamed.khaled1110@gmail.com>
2017-02-12 18:38:32 +0200

    use circleImageView

---
+++ app/src/main/java/Adapters/MyKidsAdapter.java
@@ -0,0 +1,67 @@
+package Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.bedopedia.bedopedia_android.MyKidsActivity;
+import com.example.bedopedia.bedopedia_android.R;
+
+import java.util.List;
+
+import Models.Student;
+
+/**
+ * Created by mohamed on 2/9/17.
+ */
+
+public class MyKidsAdapter extends ArrayAdapter<Student> {
+
+    public MyKidsActivity context;
+
+    public MyKidsAdapter(Context context, int resource, List<Student> items) {
+        super(context, resource, items);
+        this.context = (MyKidsActivity) context;
+    }
+
+    public static  class Holder{
+        ImageView avatar;
+        TextView name;
+        ImageButton openStudent;
+    }
+
+    @Override
+    public View getView(final int position, View view, ViewGroup parent) {
+        // Get the data item for this position
+        Student student = (Student) getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+
+        Holder item;
+        if (view == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.single_student, parent, false);
+        }
+        item=new Holder();
+
+        item.avatar = (ImageView) view.findViewById(R.id.student_avatar);
+        item.name = (TextView) view.findViewById(R.id.student_name);
+        item.openStudent = (ImageButton) view.findViewById(R.id.open_student);
+
+        item.name.setText(student.getFirstName() + " " + student.getLastName());
+        item.openStudent.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                context.itemClicked(position);
+            }
+        });
+
+        return view;
+    }
+
+}
