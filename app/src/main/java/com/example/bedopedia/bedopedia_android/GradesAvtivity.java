ec1453f20175daf21951563682977f96c0ecc7ed
Mohamed Khaled <mohamed.khaled1110@gmail.com>
2017-02-21 12:40:37 +0200

    use Asynctask and loading dialogs

--- app/src/main/java/com/example/bedopedia/bedopedia_android/GradesAvtivity.java
+++ app/src/main/java/com/example/bedopedia/bedopedia_android/GradesAvtivity.java
@@ -1,15 +1,15 @@
 package com.example.bedopedia.bedopedia_android;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.GridView;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import com.google.gson.JsonObject;
 
@@ -18,12 +18,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import Tools.Dialogue;
 import Adapters.GradesAdapter;
-import Adapters.MyKidsAdapter;
 import Models.Course;
-import Models.Student;
 import Services.ApiClient;
 import Services.ApiInterface;
+import Tools.InternetConnection;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -36,11 +36,80 @@ public class GradesAvtivity extends AppCompatActivity {
     List<Course> courses;
     ImageButton back;
     String student_id;
-    
+    ProgressDialog progress;
+    Context context;
+
+    public void loading(){
+        progress.setTitle("Loading");
+        progress.setMessage("Wait while loading...");
+    }
+
+    private class GradesAsyncTask extends AsyncTask {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            loading();
+            progress.show();
+        }
+
+        protected void onProgressUpdate(String... progress) {
+            loading();
+        }
+
+        @Override
+        protected List<Course> doInBackground(Object... param) {
+
+            SharedPreferences sharedPreferences = getSharedPreferences("cur_user", MODE_PRIVATE);
+            ApiInterface apiService = ApiClient.getClient(sharedPreferences).create(ApiInterface.class);
+            String url = "api/students/" + student_id + "/grade_certificate";
+            Map<String, String> params = new HashMap<>();
+            Call<ArrayList<JsonObject> > call = apiService.getServiseArr(url, params);
+            call.enqueue(new Callback<ArrayList<JsonObject> >() {
+                @Override
+                public void onResponse(Call<ArrayList<JsonObject>> call, Response<ArrayList<JsonObject>> response) {
+                    progress.dismiss();
+                    int statusCode = response.code();
+                    if(statusCode == 401) {
+                        Dialogue.AlertDialog(context,"Not Authorized","you don't have the right to do this");
+                    } else {
+                        if (statusCode == 200) {
+                            for (int i = 0; i < response.body().size()-1; i++) {
+                                JsonObject courseData = response.body().get(i);
+                                courses.add(new Course(
+                                        courseData.get("name").toString().substring(1,courseData.get("name").toString().length()-1),
+                                        "",
+                                        0,
+                                        courseData.get("grade").toString().substring(1,courseData.get("grade").toString().length()-1),
+                                        courseData.get("icon").toString().substring(1,courseData.get("icon").toString().length()-1)
+                                ));
+                            }
+
+                            GradesAdapter adapter = new GradesAdapter(context, R.layout.single_grade, courses);
+                            GridView gradesList = (GridView) findViewById(R.id.grades_list);
+                            gradesList.setAdapter(adapter);
+                        }
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ArrayList<JsonObject>> call, Throwable t) {
+                    progress.dismiss();
+                    Dialogue.AlertDialog(context,"Connection Failed","Check your Netwotk connection and Try again");
+                }
+            });
+            return courses;
+        }
+
+
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.grades);
+        progress = new ProgressDialog(this);
         Bundle extras= getIntent().getExtras();
         student_id = extras.getString("student_id");
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
@@ -57,49 +126,13 @@ public class GradesAvtivity extends AppCompatActivity {
         });
 
         courses = new ArrayList<Course>();
-        final Context context = this;
+        context = this;
+        if (InternetConnection.isInternetAvailable(this)){
+            new GradesAsyncTask().execute();
+        } else {
+            Dialogue.AlertDialog(this,"No NetworkConnection","Check your Netwotk connection and Try again");
+        }
 
-        SharedPreferences sharedPreferences = getSharedPreferences("cur_user", MODE_PRIVATE);
-        ApiInterface apiService = ApiClient.getClient(sharedPreferences).create(ApiInterface.class);
-        String url = "api/students/" + student_id + "/grade_certificate";
-        Map<String, String> params = new HashMap<>();
-        Call<ArrayList<JsonObject> > call = apiService.getServiseArr(url, params);
-
-        call.enqueue(new Callback<ArrayList<JsonObject> >() {
-            @Override
-            public void onResponse(Call<ArrayList<JsonObject>> call, Response<ArrayList<JsonObject>> response) {
-                int statusCode = response.code();
-                if(statusCode == 401) {
-
-                    String errorText = "wrong username or password";
-                    Toast.makeText(getApplicationContext(),errorText,Toast.LENGTH_SHORT).show();
-                } else {
-                    if (statusCode == 200) {
-                        for (int i = 0; i < response.body().size()-1; i++) {
-                            JsonObject courseData = response.body().get(i);
-                            courses.add(new Course(
-                                    courseData.get("name").toString().substring(1,courseData.get("name").toString().length()-1),
-                                    "",
-                                    0,
-                                    courseData.get("grade").toString().substring(1,courseData.get("grade").toString().length()-1),
-                                    courseData.get("icon").toString().substring(1,courseData.get("icon").toString().length()-1)
-                            ));
-                        }
-
-                        GradesAdapter adapter = new GradesAdapter(context, R.layout.single_grade, courses);
-                        GridView gradesList = (GridView) findViewById(R.id.grades_list);
-                        gradesList.setAdapter(adapter);
-
-
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<ArrayList<JsonObject>> call, Throwable t) {
-                Toast.makeText(getApplicationContext(),"connection failed",Toast.LENGTH_SHORT).show();
-            }
-        });
 
     }
 
