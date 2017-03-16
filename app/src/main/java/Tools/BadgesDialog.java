060d2793d017806a64218b37821f07cf2fef39b2
Mohamed Khaled <mohamed.khaled1110@gmail.com>
2017-03-16 09:25:45 +0200

    badges design (#47)

--- app/src/main/java/Tools/BadgesDialog.java
+++ app/src/main/java/Tools/BadgesDialog.java
@@ -2,13 +2,18 @@ package Tools;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.support.v7.app.AlertDialog;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.bedopedia.bedopedia_android.R;
 import com.example.bedopedia.bedopedia_android.StudentActivity;
+import com.example.bedopedia.bedopedia_android.TimetableActivity;
 
 import java.util.List;
 
@@ -21,16 +26,26 @@ import Models.Badge;
  */
 
 public class BadgesDialog {
+
+    public static int getInDp(int dimensionInPixel, Context context){
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dimensionInPixel, context.getResources().getDisplayMetrics());
+    }
+
     public static  void AlertDialog(Context context, List<Badge> badges){
 
         LayoutInflater inflater =  (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        Typeface robotoMedian = Typeface.createFromAsset(context.getAssets(), "font/Roboto-Medium.ttf");
 
         View v = inflater.inflate(R.layout.badges_dialog, null);
+
         BadgesAdapter badgesAdapter = new BadgesAdapter(context, R.layout.single_badge, badges);
         ListView listView = (ListView) v.findViewById(R.id.badges_list);
+        TextView title = (TextView) v.findViewById(R.id.dialog_title);
+        title.setTypeface(robotoMedian);
+
         listView.setAdapter(badgesAdapter);
 
-        new AlertDialog.Builder(context).setTitle("Badges")
+        new AlertDialog.Builder(context)
                 .setView(v)
                 .setPositiveButton("CLOSE"
                         , new DialogInterface.OnClickListener() {
