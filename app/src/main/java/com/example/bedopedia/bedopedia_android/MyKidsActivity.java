d6e1b84552cd0474c7069623bf48badaa217d07b
Mohamed Khaled <mohamed.khaled1110@gmail.com>
2017-03-28 17:17:28 +0200

    create menu and enable logot

--- app/src/main/java/com/example/bedopedia/bedopedia_android/MyKidsActivity.java
+++ app/src/main/java/com/example/bedopedia/bedopedia_android/MyKidsActivity.java
@@ -13,6 +13,8 @@ import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -20,8 +22,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -29,6 +32,9 @@ import android.widget.TextView;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -49,7 +55,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 
-public class MyKidsActivity extends AppCompatActivity{
+public class MyKidsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     ArrayList<Student> myKids;
     String id;
@@ -59,9 +65,10 @@ public class MyKidsActivity extends AppCompatActivity{
     public static Integer notificationNumber = 0;
     TextView notificationNuber;
 
-    DrawerLayout notificationLayout;
+    DrawerLayout drawer;
     ListView notificationList;
     ActionBarDrawerToggle notificationToggle;
+    ActionBarDrawerToggle mainToggle;
     List<NotificationModel> notifications;
 
     private RecyclerView mRecyclerView;
@@ -93,6 +100,7 @@ public class MyKidsActivity extends AppCompatActivity{
             SharedPreferences sharedPreferences = getSharedPreferences("cur_user", MODE_PRIVATE);
             ApiInterface apiService = ApiClient.getClient(sharedPreferences).create(ApiInterface.class);
             id = sharedPreferences.getString("id", "");
+
             String url = "api/parents/" + id + "/children";
             Map <String, String> params = new HashMap<>();
             params.put("parent_id" , id);
@@ -298,8 +306,27 @@ public class MyKidsActivity extends AppCompatActivity{
         progress = new ProgressDialog(this);
         kidsAttendances = new ArrayList<JsonArray>();
 
+        SharedPreferences sharedPreferences = getSharedPreferences("cur_user", MODE_PRIVATE);
+        String email = sharedPreferences.getString("email", "");
+        String avatarUrl = sharedPreferences.getString("avatar_url", "");
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View hView =  navigationView.getHeaderView(0);
+
+        TextView emailView = (TextView) hView.findViewById(R.id.my_email);
+        ImageView myAvatar = (ImageView) hView.findViewById(R.id.my_avatar_photo);
+
+        Log.e("here" , email);
+        Log.e("here" , avatarUrl);
+
+         emailView.setText(email);
+        Picasso.with(this).load(ApiClient.BASE_URL+avatarUrl).into(myAvatar);
+
+
+
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.home_actionbar);
+
         Typeface roboto = Typeface.createFromAsset(context.getAssets(), "font/Roboto-Medium.ttf"); //use this.getAssets if you are calling from an Activity
 
         TextView title = (TextView) findViewById(R.id.home_action_bar_title);
@@ -317,14 +344,21 @@ public class MyKidsActivity extends AppCompatActivity{
 
         RelativeLayout notificationButton =  (RelativeLayout) findViewById(R.id.relative_layout);
 
-        notificationLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mainToggle = new ActionBarDrawerToggle(
+                this, drawer, null , R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(mainToggle);
+        mainToggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
         notificationList = (ListView) findViewById(R.id.listview_notification);
-        notificationLayout.setDrawerListener(notificationToggle);
+        drawer.setDrawerListener(notificationToggle);
         notificationButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if(notificationLayout.isDrawerOpen(notificationList)){
-                    notificationLayout.closeDrawer(notificationList);
+                if(drawer.isDrawerOpen(notificationList)){
+                    drawer.closeDrawer(notificationList);
                     TextView title = (TextView) findViewById(R.id.home_action_bar_title);
                     Typeface roboto = Typeface.createFromAsset(context.getAssets(), "font/Roboto-Medium.ttf"); //use this.getAssets if you are calling from an Activity
                     title.setTypeface(roboto);
@@ -338,7 +372,7 @@ public class MyKidsActivity extends AppCompatActivity{
                         notificationNumber = 0;
                         changeTheNotificationNumber();
                         new MarkAllAsSeenAsyncTask().execute();
-                        notificationLayout.openDrawer(notificationList);
+                        drawer.openDrawer(notificationList);
                 }
             }
         });
@@ -359,7 +393,7 @@ public class MyKidsActivity extends AppCompatActivity{
                     notificationNumberText.setVisibility(View.VISIBLE);
                 }
 
-                if(notificationLayout.isDrawerOpen(notificationList)){
+                if(drawer.isDrawerOpen(notificationList)){
                     TextView title = (TextView) findViewById(R.id.home_action_bar_title);
                     title.setText("Notifications");
                 } else {
@@ -387,4 +421,37 @@ public class MyKidsActivity extends AppCompatActivity{
 //        Toast.makeText(getApplicationContext(),"you clicked " + myKids.get(index).getFirstName(),
 //        Toast.LENGTH_LONG).show();
     }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_logout) {
+//            SharedPreferences sharedPreferences = getSharedPreferences("cur_user", MODE_PRIVATE);
+//            SharedPreferences.Editor editor = sharedPreferences.edit();
+//            editor.putString("Base_Url", "");
+//            editor.putString("header_access-token", "");
+//            editor.putString("user_data", "");
+//            editor.putString("header_uid", "");
+//            editor.commit();
+
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
