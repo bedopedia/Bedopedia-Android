1cefeb5b3eef8650a7207c7a860bde03eb092ee6
Mohamed Khaled <mohamed.khaled1110@gmail.com>
2017-04-10 08:23:30 +0200

    separate timetable module

--- app/src/main/java/timetable/TimetableActivity.java
+++ app/src/main/java/timetable/TimetableActivity.java
@@ -1,50 +1,18 @@
-package com.example.bedopedia.bedopedia_android;
+package timetable;
 
-import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Layout;
-import android.util.TypedValue;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.google.gson.JsonObject;
+import com.example.bedopedia.bedopedia_android.R;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.TimeZone;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import Adapters.TimetableAdapter;
-import Fragments.TodayFragment;
-import Models.TimetableSlot;
-import Services.ApiClient;
-import Services.ApiInterface;
-import Tools.Dialogue;
-import Tools.InternetConnection;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import timetable.TimetableAdapter;
 
 
 /**
