762313c5023a61c62562d381e11f9773d2d6f9d5
AliAbdElrahman74 <aliabdelrahmanweka74@gmail.com>
2017-04-12 10:00:39 +0200

    make real time work and synchronized with web (#73)
    * make real time work and synced
    
    * remove tags from the body
    
    * remove logs
    
    * remove log

--- app/src/main/java/com/example/bedopedia/bedopedia_android/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/bedopedia/bedopedia_android/MyFirebaseMessagingService.java
@@ -9,6 +9,7 @@ import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.NotificationCompat;
 
 
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -31,19 +32,44 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         if (sharedPreferences.getString("is_logged_in", "").equals("true")) {
             Map<String, String> data = remoteMessage.getData();
 
+            if (data.get("notification_type").equals("notification")) {
+                if (data.get("message").equals("markAsSeen")) {
+                    MyKidsActivity.notificationNumber = 0;
+                    NotificationManager nMgr = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    nMgr.cancelAll();
+                } else {
+                    MyKidsActivity.notificationNumber = MyKidsActivity.notificationNumber + 1;
+                    android.support.v4.app.NotificationCompat.Builder mBuilder =
+                            new NotificationCompat.Builder(this)
+                                    .setSmallIcon(R.drawable.quizzes_ico)
+                                    .setContentTitle(data.get("title"))
+                                    .setContentText(data.get("message"));
+                    Intent resultIntent = new Intent(this, MyKidsActivity.class);
 
-            if (data.get("message").equals("markAsSeen")) {
-                MyKidsActivity.notificationNumber = 0;
-                NotificationManager nMgr = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                nMgr.cancelAll();
-            } else {
-                MyKidsActivity.notificationNumber = MyKidsActivity.notificationNumber + 1;
+                    TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+                    stackBuilder.addParentStack(MyKidsActivity.class);
+                    stackBuilder.addNextIntent(resultIntent);
+                    PendingIntent resultPendingIntent =
+                            stackBuilder.getPendingIntent(
+                                    0,
+                                    PendingIntent.FLAG_UPDATE_CURRENT
+                            );
+                    mBuilder.setContentIntent(resultPendingIntent);
+                    NotificationManager mNotificationManager =
+                            (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    mNotificationManager.notify(mdl, mBuilder.build());
+                    mdl = mdl +1;
+                }
+
+
+            } else if (data.get("notification_type").equals("message")){
+                StudentActivity.messageNumber = StudentActivity.messageNumber + 1;
                 android.support.v4.app.NotificationCompat.Builder mBuilder =
                         new NotificationCompat.Builder(this)
                                 .setSmallIcon(R.drawable.quizzes_ico)
-                                .setContentTitle(data.get("title"))
-                                .setContentText(data.get("message"));
-                Intent resultIntent = new Intent(this, MyKidsActivity.class);
+                                .setContentTitle(data.get("name"))
+                                .setContentText(android.text.Html.fromHtml(data.get("message")));
+                Intent resultIntent = new Intent(this, AskTeacherActivity.class);
 
                 TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
                 stackBuilder.addParentStack(MyKidsActivity.class);
@@ -59,10 +85,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 mNotificationManager.notify(mdl, mBuilder.build());
                 mdl = mdl +1;
             }
+            }
 
 
-        } else {
         }
-
     }
-}
