0cf130940c6696ae72b5c5b01650cd8f3e9a8e0d
AliAbdElrahman74 <aliabdelrahmanweka74@gmail.com>
2017-06-18 15:37:25 +0300

    fix real time for notifications and messages (#162)
    * fix real time for notifications and messages
    
    * change var name

--- app/src/main/java/com/example/bedopedia/bedopedia_android/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/bedopedia/bedopedia_android/MyFirebaseMessagingService.java
@@ -13,6 +13,9 @@ import android.support.v7.app.NotificationCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Map;
 
 import myKids.MyKidsActivity;
@@ -29,23 +32,29 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
-
         SharedPreferences sharedPreferences = getSharedPreferences("cur_user", MODE_PRIVATE);
         if (sharedPreferences.getString("is_logged_in", "").equals("true")) {
             Map<String, String> data = remoteMessage.getData();
 
-            if (data.get("notification_type").equals("notification")) {
-                if (data.get("message").equals("markAsSeen")) {
+
+            if (data.get("event").equals("notification")) {
+
+               /* if (data.get("message").equals("markAsSeen")) {
                     MyKidsActivity.notificationNumber = 0;
                     NotificationManager nMgr = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
                     nMgr.cancelAll();
                 } else {
+
+                }
+                */
+                try {
+                    JSONObject notification = new JSONObject(data.get("payload"));
                     MyKidsActivity.notificationNumber = MyKidsActivity.notificationNumber + 1;
                     android.support.v4.app.NotificationCompat.Builder mBuilder =
                             new NotificationCompat.Builder(this)
                                     .setSmallIcon(R.drawable.quizzes_ico)
-                                    .setContentTitle(data.get("title"))
-                                    .setContentText(data.get("message"));
+                                    .setContentTitle(notification.getString("message"))
+                                    .setContentText(notification.getString("text"));
                     Intent resultIntent = new Intent(this, MyKidsActivity.class);
 
                     TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
@@ -61,34 +70,48 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                             (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
                     mNotificationManager.notify(mdl, mBuilder.build());
                     mdl = mdl +1;
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
 
+                //}
 
-            } else if (data.get("notification_type").equals("message")){
-                StudentFragment.messageNumber = StudentFragment.messageNumber + 1;
-                android.support.v4.app.NotificationCompat.Builder mBuilder =
-                        new NotificationCompat.Builder(this)
-                                .setSmallIcon(R.drawable.quizzes_ico)
-                                .setContentTitle(data.get("name"))
-                                .setContentText(android.text.Html.fromHtml(data.get("message")));
-                Intent resultIntent = new Intent(this, AskTeacherActivity.class);
 
-                TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-                stackBuilder.addParentStack(MyKidsActivity.class);
-                stackBuilder.addNextIntent(resultIntent);
-                PendingIntent resultPendingIntent =
-                        stackBuilder.getPendingIntent(
-                                0,
-                                PendingIntent.FLAG_UPDATE_CURRENT
-                        );
-                mBuilder.setContentIntent(resultPendingIntent);
-                NotificationManager mNotificationManager =
-                        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                mNotificationManager.notify(mdl, mBuilder.build());
-                mdl = mdl +1;
+            } else if (data.get("event").equals("message")){
+                try {
+                    JSONObject message = new JSONObject(data.get("payload"));
+                    JSONObject user = new JSONObject(message.getString("user"));
+                    StudentFragment.messageNumber = StudentFragment.messageNumber + 1;
+                    android.support.v4.app.NotificationCompat.Builder mBuilder =
+                            new NotificationCompat.Builder(this)
+                                    .setSmallIcon(R.drawable.quizzes_ico)
+                                    .setContentTitle(user.getString("name"))
+                                    .setContentText(android.text.Html.fromHtml(message.getString("body")));
+                    Intent resultIntent = new Intent(this, AskTeacherActivity.class);
+
+                    TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+                    stackBuilder.addParentStack(MyKidsActivity.class);
+                    stackBuilder.addNextIntent(resultIntent);
+                    PendingIntent resultPendingIntent =
+                            stackBuilder.getPendingIntent(
+                                    0,
+                                    PendingIntent.FLAG_UPDATE_CURRENT
+                            );
+                    mBuilder.setContentIntent(resultPendingIntent);
+                    NotificationManager mNotificationManager =
+                            (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    mNotificationManager.notify(mdl, mBuilder.build());
+                    mdl = mdl +1;
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
             }
-            }
-
 
         }
+
+
     }
+}
