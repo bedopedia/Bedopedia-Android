cd70755c56e1c94d3f23372a40de7b521dcdcc2a
Ali Abdelrahman <aliabdelrahmanweka74@gmail.com>
2017-06-19 10:26:08 +0200

    call fragment from activity

--- app/src/main/java/com/example/bedopedia/bedopedia_android/AskTeacherActivity.java
+++ app/src/main/java/com/example/bedopedia/bedopedia_android/AskTeacherActivity.java
@@ -1,40 +1,12 @@
 package com.example.bedopedia.bedopedia_android;
 
-import android.app.ProgressDialog;
-import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Pair;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.view.MenuItem;
+import Tools.FragmentUtils;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonNull;
-import com.google.gson.JsonObject;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import Adapters.AskTeacherAdapter;
-import Models.Message;
-import Models.MessageThread;
-import Tools.SharedPreferenceUtils;
-import myKids.Student;
-import Models.User;
-import login.Services.ApiClient;
-import login.Services.ApiInterface;
-import Tools.Dialogue;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-import static timetable.TimetableActivity.context;
 
 
 public class AskTeacherActivity extends AppCompatActivity {
@@ -50,7 +22,16 @@ public class AskTeacherActivity extends AppCompatActivity {
         ActionBar askTeacherActionbar = getSupportActionBar();
         askTeacherActionbar.setDisplayHomeAsUpEnabled(true);
         askTeacherActionbar.setTitle(R.string.AskTeacherTitle);
+        FragmentUtils.createFragment(getSupportFragmentManager(), AskTeacherFragment.newInstance(this), R.id.askTeacherContainer);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()  == android.R.id.home) {
+            onBackPressed();
+        }
+        return true;
     }
 
 }
