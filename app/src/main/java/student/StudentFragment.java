e5b407366a9e2117b6fd9d5887b823d195b1ec50
Ali Abdelrahman <aliabdelrahmanweka74@gmail.com>
2017-07-05 16:09:49 +0200

    remove log

--- app/src/main/java/student/StudentFragment.java
+++ app/src/main/java/student/StudentFragment.java
@@ -20,7 +20,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Spannable;
 import android.text.SpannableString;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
