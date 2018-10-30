502821d77f4c866db3f23391183dd7dd41d73767
mohamed gamal <mohamed11gamal96@gmail.com>
2018-10-30 12:06:35 +0200

    parsed default config JSON Array
    -also added getters in School model and made the field private
    - fixed btn from both screen id issue

--- app/src/main/java/trianglz/models/School.java
+++ app/src/main/java/trianglz/models/School.java
@@ -6,15 +6,15 @@ import java.io.Serializable;
  * This file is spawned by Gemy on 10/29/2018.
  */
 public class School implements Serializable {
-    int id;
-    String name;
-    String schoolDescription;
-    String AvatarUrl;
-    String gaTrackingId;
-    String attendanceAllowSlot;
-    String attendanceAllowFullDay;
-    String defaultConfigSlot;
-    String defaultConfigFullDay;
+    private int id;
+    private String name;
+    private String schoolDescription;
+    private String AvatarUrl;
+    private String gaTrackingId;
+    private String attendanceAllowSlot;
+    private String attendanceAllowFullDay;
+    private String defaultConfigSlot;
+    private String defaultConfigFullDay;
 
     public School(int id, String name, String schoolDescription, String avatarUrl, String gaTrackingId, String attendanceAllowSlot, String attendanceAllowFullDay, String defaultConfigSlot, String defaultConfigFullDay) {
         this.id = id;
@@ -27,5 +27,42 @@ public class School implements Serializable {
         this.defaultConfigSlot = defaultConfigSlot;
         this.defaultConfigFullDay = defaultConfigFullDay;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSchoolDescription() {
+        return schoolDescription;
+    }
+
+    public String getAvatarUrl() {
+        return AvatarUrl;
+    }
+
+    public String getGaTrackingId() {
+        return gaTrackingId;
+    }
+
+    public String getAttendanceAllowSlot() {
+        return attendanceAllowSlot;
+    }
+
+    public String getAttendanceAllowFullDay() {
+        return attendanceAllowFullDay;
+    }
+
+    public String getDefaultConfigSlot() {
+        return defaultConfigSlot;
+    }
+
+    public String getDefaultConfigFullDay() {
+        return defaultConfigFullDay;
+    }
+
 
 }
