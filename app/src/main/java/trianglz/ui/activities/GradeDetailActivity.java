a1f6f54931929419d6685e4aefbc3d32b101e473
Aly Abd-El Shafy <alyabdelshafy6561@gmail.com>
2018-11-07 16:39:56 +0200

    add item quarter

--- app/src/main/java/trianglz/ui/activities/GradeDetailActivity.java
+++ app/src/main/java/trianglz/ui/activities/GradeDetailActivity.java
@@ -2,14 +2,31 @@ package trianglz.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.skolera.skolera_android.R;
 
+import trianglz.models.CourseGroup;
+import trianglz.ui.adapters.GradeDetailAdapter;
+import trianglz.utils.Constants;
+
 public class GradeDetailActivity extends AppCompatActivity {
-    
+    CourseGroup courseGroup;
+    private RecyclerView recyclerView;
+    private GradeDetailAdapter gradeDetailAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grade_detail);
+        getValueFromIntent();
+        bindViews();
+    }
+
+    private void bindViews() {
+        recyclerView = findViewById(R.id.recycler_view);
+    }
+
+    private void getValueFromIntent() {
+        courseGroup = (CourseGroup) getIntent().getBundleExtra(Constants.KEY_BUNDLE).getSerializable(Constants.KEY_COURSE_GROUPS);
     }
 }
---
+++ app/src/main/java/trianglz/ui/adapters/GradeDetailAdapter.java
@@ -0,0 +1,14 @@
+package trianglz.ui.adapters;
+
+import android.content.Context;
+
+/**
+ * Created by ${Aly} on 11/7/2018.
+ */
+public class GradeDetailAdapter {
+    private Context context;
+
+    public GradeDetailAdapter(Context context) {
+        this.context = context;
+    }
+}
