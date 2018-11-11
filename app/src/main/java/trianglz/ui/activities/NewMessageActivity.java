0f6ae9c60db4f28235fea424ceefcecca3cbacc2
Aly Abd-El Shafy <alyabdelshafy6561@gmail.com>
2018-11-11 20:51:15 +0200

    show subjects in new message

--- app/src/main/java/trianglz/ui/activities/NewMessageActivity.java
+++ app/src/main/java/trianglz/ui/activities/NewMessageActivity.java
@@ -2,9 +2,11 @@ package trianglz.ui.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.skolera.skolera_android.R;
 
@@ -16,14 +18,18 @@ import trianglz.managers.SessionManager;
 import trianglz.managers.api.ApiEndPoints;
 import trianglz.models.Student;
 import trianglz.models.Subject;
+import trianglz.ui.adapters.NewMessageAdapter;
 import trianglz.utils.Constants;
 import trianglz.utils.Util;
 
-public class NewMessageActivity extends SuperActivity implements View.OnClickListener,NewMessagePresenter {
+public class NewMessageActivity extends SuperActivity implements View.OnClickListener,
+        NewMessagePresenter,NewMessageAdapter.NewMessageAdapterInterface {
     private Student student;
     private ImageButton backBtn;
     private RecyclerView recyclerView;
     private NewMessageView newMessageView;
+    private NewMessageAdapter newMessageAdapter;
+    private TextView subjectHeaderTextView,teacherHeaderTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +51,11 @@ public class NewMessageActivity extends SuperActivity implements View.OnClickLis
         backBtn = findViewById(R.id.btn_back);
         recyclerView = findViewById(R.id.recycler_view);
         newMessageView = new NewMessageView(this,this);
+        newMessageAdapter = new NewMessageAdapter(this,this);
+        recyclerView.setAdapter(newMessageAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        subjectHeaderTextView = findViewById(R.id.tv_header_subject);
+        teacherHeaderTextView = findViewById(R.id.tv_header_teacher);
     }
 
     private void setListeners() {
@@ -66,11 +77,23 @@ public class NewMessageActivity extends SuperActivity implements View.OnClickLis
 
     @Override
     public void onGetCourseGroupsSuccess(ArrayList<Subject> subjectArrayList) {
+        newMessageAdapter.addData(subjectArrayList);
         progress.dismiss();
     }
 
     @Override
     public void onGetCourseGroupsFailure(String message, int errorCode) {
         progress.dismiss();
+    }
+
+    @Override
+    public void onSubjectSelected(int position) {
+        if(newMessageAdapter.mDataList.get(position).courseName.equals(subjectHeaderTextView.getText().toString())){
+            teacherHeaderTextView.setVisibility(View.GONE);
+            subjectHeaderTextView.setText(getResources().getString(R.string.select_a_course));
+        }else {
+            subjectHeaderTextView.setText(newMessageAdapter.mDataList.get(position).courseName);
+            teacherHeaderTextView.setVisibility(View.VISIBLE);
+        }
     }
 }
--- app/src/main/java/trianglz/ui/adapters/NewMessageAdapter.java
+++ app/src/main/java/trianglz/ui/adapters/NewMessageAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -34,12 +35,29 @@ public class NewMessageAdapter extends RecyclerView.Adapter<NewMessageAdapter.Ho
     @Override
     public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(context).inflate(R.layout.item_grade, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.item_subject, parent, false);
         return new Holder(view);
     }
 
     @Override
-    public void onBindViewHolder(Holder holder, final int position) {
+    public void onBindViewHolder(final Holder holder, final int position) {
+        Subject subject = mDataList.get(position);
+        String teacherCount = "";
+        if(subject.teacherArrayList.size()>1){
+           teacherCount = (subject.teacherArrayList.size()+" " +
+                   context.getResources().getString(R.string.teacher));
+        }else {
+            teacherCount = (subject.teacherArrayList.size()+" "+
+                    context.getResources().getString(R.string.teacher));
+        }
+        holder.teacherCountTextView.setText(teacherCount);
+        holder.subjectNameTextView.setText(subject.courseName);
+        holder.itemLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newMessageAdapterInterface.onSubjectSelected(holder.getAdapterPosition());
+            }
+        });
     }
 
     @Override
@@ -55,10 +73,14 @@ public class NewMessageAdapter extends RecyclerView.Adapter<NewMessageAdapter.Ho
 
     public static class Holder extends RecyclerView.ViewHolder {
 
-
+        public TextView subjectNameTextView,teacherCountTextView;
+        public LinearLayout itemLayout;
 
         public Holder(View itemView) {
             super(itemView);
+            subjectNameTextView = itemView.findViewById(R.id.tv_subject_name);
+            teacherCountTextView = itemView.findViewById(R.id.tv_teacher_count);
+            itemLayout = itemView.findViewById(R.id.item_layout);
 
         }
     }
