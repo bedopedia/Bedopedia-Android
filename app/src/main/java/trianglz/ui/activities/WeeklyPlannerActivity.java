c9fa440e1c77c24b135dd02f9b1cdcc4b879f75a
mohamed gamal <mohamed11gamal96@gmail.com>
2019-01-16 14:22:00 +0200

    added tab layout and rounded background

--- app/src/main/java/trianglz/ui/activities/WeeklyPlannerActivity.java
+++ app/src/main/java/trianglz/ui/activities/WeeklyPlannerActivity.java
@@ -3,13 +3,17 @@ package trianglz.ui.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
 import com.skolera.skolera_android.R;
 
 public class WeeklyPlannerActivity extends AppCompatActivity {
-
+    private SmartTabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekly_planner);
+    }
+    private void bindViews () {
+        tabLayout = findViewById(R.id.tab_layout);
     }
 }
