70fc1544b0955878fa5bb7ad64259e90c9115b46
mohamed gamal <mohamed11gamal96@gmail.com>
2019-01-17 12:49:35 +0200

    updated ui for weekly-planner activity
    - added a fragment for the view pager
    - added sfui_medium font
    - added the adapter for the view pager
    - changed some attributes for the smart tab layout

--- app/src/main/java/trianglz/ui/activities/WeeklyPlannerActivity.java
+++ app/src/main/java/trianglz/ui/activities/WeeklyPlannerActivity.java
@@ -1,19 +1,44 @@
 package trianglz.ui.activities;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
 import com.skolera.skolera_android.R;
 
+import java.util.ArrayList;
+
+import trianglz.ui.adapters.WeeklyPlannerAdapter;
+import trianglz.ui.fragments.DayFragment;
+
 public class WeeklyPlannerActivity extends AppCompatActivity {
     private SmartTabLayout tabLayout;
+    private ViewPager viewPager;
+    private WeeklyPlannerAdapter adapter;
+    private DayFragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekly_planner);
+        bindViews();
     }
     private void bindViews () {
         tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.viewpager);
+        adapter = new WeeklyPlannerAdapter(getSupportFragmentManager(), this);
+        adapter.addFragmentArrayList(getFragmentList());
+        viewPager.setAdapter(adapter);
+        tabLayout.setViewPager(viewPager);
     }
+    private ArrayList<DayFragment> getFragmentList(){
+        ArrayList<DayFragment> fragmentArrayList = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            fragment = new DayFragment();
+            fragmentArrayList.add(fragment);
+        }
+        return fragmentArrayList;
+    }
+
+
 }
---
+++ app/src/main/java/trianglz/ui/fragments/DayFragment.java
@@ -0,0 +1,31 @@
+package trianglz.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.skolera.skolera_android.R;
+
+public class DayFragment extends Fragment {
+    public DayFragment(){}
+
+    View rootView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate((R.layout.fragment_day), container, false);
+        return rootView;
+
+    }
+}
