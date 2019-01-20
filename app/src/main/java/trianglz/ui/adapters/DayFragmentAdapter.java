afe2c2d4c11f278a7ca04ea83f909393b3fccccf
mohamed gamal <mohamed11gamal96@gmail.com>
2019-01-20 14:20:58 +0200

    added recycler view for the days in the view pager
    - created DailyNote model
    - added adapters and integrated them in the Fragment file

---
+++ app/src/main/java/trianglz/ui/adapters/DayFragmentAdapter.java
@@ -0,0 +1,121 @@
+package trianglz.ui.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.skolera.skolera_android.R;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import agency.tango.android.avatarview.IImageLoader;
+import agency.tango.android.avatarview.loader.PicassoLoader;
+import agency.tango.android.avatarview.views.AvatarView;
+import trianglz.components.AvatarPlaceholderModified;
+import trianglz.components.CircleTransform;
+import trianglz.models.DailyNote;
+
+/**
+ * This file is spawned by Gemy on 1/20/2019.
+ */
+public class DayFragmentAdapter extends RecyclerView.Adapter<DayFragmentAdapter.Holder> {
+    public Context context;
+    public List<DailyNote> mDataList;
+
+
+    public DayFragmentAdapter(Context context) {
+        this.context = context;
+        this.mDataList = new ArrayList<>();
+    }
+
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.item_grade, parent, false);
+        return new Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+        DailyNote dailyNote = mDataList.get(position);
+        holder.subjectNameTextView.setText(dailyNote.getSubjectName());
+        holder.gradeTextView.setVisibility(View.GONE);
+        setSubjectName(null,getSubjectNameForPlaceHolder(dailyNote.getSubjectName()),holder);
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mDataList.size();
+    }
+
+    public void addData(List<DailyNote> dailyNoteList) {
+        this.mDataList.clear();
+        this.mDataList.addAll(dailyNoteList);
+        notifyDataSetChanged();
+    }
+
+    public static class Holder extends RecyclerView.ViewHolder {
+
+        private TextView subjectNameTextView,gradeTextView;
+        private AvatarView subjectImageView;
+        private LinearLayout itemLayout;
+        public IImageLoader imageLoader;
+
+        private Holder(View itemView) {
+            super(itemView);
+            gradeTextView = itemView.findViewById(R.id.tv_grade);
+            subjectNameTextView = itemView.findViewById(R.id.tv_subject_name);
+            subjectImageView = itemView.findViewById(R.id.img_subject);
+            itemLayout = itemView.findViewById(R.id.item_layout);
+            imageLoader = new PicassoLoader();
+        }
+    }
+
+
+
+    private void setSubjectName(String imageUrl, final String name, final DayFragmentAdapter.Holder holder) {
+        if (imageUrl == null || imageUrl.equals("")) {
+            holder.imageLoader = new PicassoLoader();
+            holder.imageLoader.loadImage(holder.subjectImageView, new AvatarPlaceholderModified(name), "Path of Image");
+        } else {
+            Picasso.with(context)
+                    .load(imageUrl)
+                    .fit()
+                    .transform(new CircleTransform())
+                    .into(holder.subjectImageView, new Callback() {
+                        @Override
+                        public void onSuccess() {
+
+                        }
+
+                        @Override
+                        public void onError() {
+                            holder.imageLoader = new PicassoLoader();
+                            holder.imageLoader.loadImage(holder.subjectImageView, new AvatarPlaceholderModified(name), "Path of Image");
+                        }
+                    });
+        }
+    }
+
+
+    private String getSubjectNameForPlaceHolder(String name){
+
+        if(name.indexOf('&') != -1){
+            int indexOfAnd = name.indexOf('&');
+            name = name.substring(0, indexOfAnd-1) + name.substring(indexOfAnd+1);
+        }
+
+        return name;
+    }
+
+}
--- app/src/main/java/trianglz/ui/fragments/DayFragment.java
+++ app/src/main/java/trianglz/ui/fragments/DayFragment.java
@@ -4,28 +4,50 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.skolera.skolera_android.R;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import trianglz.models.DailyNote;
+import trianglz.ui.adapters.DayFragmentAdapter;
+
 public class DayFragment extends Fragment {
-    public DayFragment(){}
 
-    View rootView;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
+    private RecyclerView recyclerView;
+    private View rootView;
+    private DayFragmentAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate((R.layout.fragment_day), container, false);
+        bindViews();
         return rootView;
 
+    }
+    private void bindViews(){
+        adapter = new DayFragmentAdapter(getActivity());
+        recyclerView = rootView.findViewById(R.id.recycler_view);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false));
+        adapter.addData(createDummyData());
+    }
+
+    private List<DailyNote> createDummyData() {
+        List<DailyNote> list = new ArrayList<>();
+        for (int i = 0; i < 11; i++) {
+            DailyNote note = new DailyNote("Arabic" + i, "",
+                    "", "", new Date(),"");
+            list.add(note);
+        }
+        return list;
     }
 }
