734443bf7c1b5fc5088d0f0e34bf4bd4b6b3133c
Mohamed Gamal <mohamed11gamal96@gmail.com>
2019-07-17 12:52:12 +0200

    Add new 4 empty Fragments

---
+++ app/src/main/java/trianglz/ui/fragments/AnnouncementsFragment.java
@@ -0,0 +1,30 @@
+package trianglz.ui.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.skolera.skolera_android.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AnnouncementsFragment extends Fragment {
+
+
+    public AnnouncementsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_announcements, container, false);
+    }
+
+}
