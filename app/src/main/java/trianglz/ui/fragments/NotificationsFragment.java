63c0625523862fdefa04191a3f27437af28c4b51
Mohamed Gamal <mohamed11gamal96@gmail.com>
2019-07-17 14:26:22 +0200

    Add notification fragment

--- app/src/main/java/trianglz/ui/fragments/NotificationsFragment.java
+++ app/src/main/java/trianglz/ui/fragments/NotificationsFragment.java
@@ -3,17 +3,43 @@ package trianglz.ui.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.skolera.skolera_android.R;
+
+import java.util.ArrayList;
+
+import trianglz.core.presenters.AdapterPaginationInterface;
+import trianglz.core.presenters.NotificationsPresenter;
+import trianglz.core.views.NotificationsView;
+import trianglz.managers.SessionManager;
+import trianglz.managers.api.ApiEndPoints;
+import trianglz.models.Notification;
+import trianglz.ui.activities.StudentMainActivity;
+import trianglz.ui.adapters.NotificationsAdapter;
+import trianglz.utils.Util;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NotificationsFragment extends Fragment {
+public class NotificationsFragment extends Fragment implements NotificationsPresenter, View.OnClickListener, AdapterPaginationInterface {
+    
+    // fragment root view 
+    private View rootView;
 
+    // parent activity
+    private StudentMainActivity activity;
+    private RecyclerView recyclerView;
+    private NotificationsAdapter adapter;
+    private Button closeBtn;
+    private NotificationsView notificationsView;
+    private int pageNumber;
+    private boolean newIncomingNotificationData;
 
     public NotificationsFragment() {
         // Required empty public constructor
@@ -23,8 +49,91 @@ public class NotificationsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+        // Inflate the layout for getActivity() fragment
+        rootView = inflater.inflate(R.layout.fragment_notifications, container, false);
+        activity = (StudentMainActivity) getActivity(); 
+        bindViews();
+        setListeners();
+        String url = SessionManager.getInstance().getBaseUrl() + ApiEndPoints.setAsSeen(SessionManager.getInstance().getUserId());
+        notificationsView.setAsSeen(url);
+        SessionManager.getInstance().setNotificationCounterToZero();
+        if(Util.isNetworkAvailable(getActivity())){
+            getNotifications(false);
+            activity.showLoadingDialog();
+        }else {
+            Util.showNoInternetConnectionDialog(getActivity());
+        }
+        return rootView;
+    }
+    private void bindViews() {
+        pageNumber = 1;
+        recyclerView = rootView.findViewById(R.id.recycler_view);
+        adapter = new NotificationsAdapter(getActivity(), this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        notificationsView = new NotificationsView(getActivity(), this);
+        closeBtn = rootView.findViewById(R.id.btn_close);
     }
 
+    private void setListeners() {
+        closeBtn.setOnClickListener(this);
+    }
+
+    private void getNotifications(boolean pagination) {
+        if (Util.isNetworkAvailable(getActivity())) {
+            activity.showLoadingDialog();
+            String url = SessionManager.getInstance().getBaseUrl() + "/api/users/" +
+                    SessionManager.getInstance().getUserId() + "/notifications";
+            if (!pagination) {
+                pageNumber = 1;
+            }
+            notificationsView.getNotifications(url, pageNumber,20);
+        } else {
+            Util.showNoInternetConnectionDialog(getActivity());
+        }
+    }
+
+    @Override
+    public void onGetNotificationSuccess(ArrayList<Notification> notifications) {
+        if (activity.progress.isShowing()) {
+            activity.progress.dismiss();
+        }
+        newIncomingNotificationData = notifications.size() != 0;
+        adapter.addData(notifications, newIncomingNotificationData);
+    }
+
+    @Override
+    public void onGetNotificationFailure(String message, int errorCode) {
+        if(activity.progress.isShowing()){
+            activity.progress.dismiss();
+        }
+        if(errorCode == 401 || errorCode == 500 ){
+            activity.logoutUser(getActivity());
+        }else {
+            activity.showErrorDialog(getActivity());
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_close:
+                activity.finish();
+                break;
+        }
+    }
+
+    @Override
+    public void onReachPosition() {
+        pageNumber++;
+        getNotifications(true);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(activity.progress.isShowing()){
+            activity.progress.dismiss();
+        }
+    }
 }
