bbf55bcfd1c2c8cafc641cb0bcf0eed81d2d0998
Mohamed Gamal <mohamed11gamal96@gmail.com>
2019-07-17 12:23:24 +0200

    Create new fragment host activity
    Relocated the newly added bottom tab layout to the new activity. Removed it from the old activity (to be Fragment)

---
+++ app/src/main/java/trianglz/ui/activities/StudentMainActivity.java
@@ -0,0 +1,18 @@
+package trianglz.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.skolera.skolera_android.R;
+
+import java.util.Objects;
+
+public class StudentMainActivity extends SuperActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_main);
+    }
+}
