1992b7d8921d2292007710b285a97abc7fa1db9d
Mohamed Gamal <mohamed11gamal96@gmail.com>
2019-07-18 11:52:51 +0200

    Prepare posts adapter

--- app/src/main/java/trianglz/ui/adapters/PostsAdapter.java
+++ app/src/main/java/trianglz/ui/adapters/PostsAdapter.java
@@ -1,7 +1,9 @@
 package trianglz.ui.adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -10,22 +12,40 @@ import android.widget.TextView;
 
 import com.skolera.skolera_android.R;
 
+import java.util.ArrayList;
+
+import trianglz.models.PostsResponse;
+
 public class PostsAdapter extends RecyclerView.Adapter {
+    private Context context;
+    ArrayList<PostsResponse> postsResponses;
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.item_posts, parent, false);
+        return new PostsViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        PostsViewHolder viewHolder = (PostsViewHolder) holder;
 
     }
 
+    public PostsAdapter(Context context) {
+        postsResponses = new ArrayList<>();
+        this.context = context;
+    }
     @Override
     public int getItemCount() {
         return 0;
     }
+    public void addData(ArrayList<PostsResponse> donations) {
+        postsResponses.clear();
+        postsResponses.addAll(donations);
+        notifyDataSetChanged();
+    }
+
 
     public class PostsViewHolder extends RecyclerView.ViewHolder {
 
