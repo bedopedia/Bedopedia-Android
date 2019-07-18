c30567274ed9eeba145d85ac25ada74d8066dff8
Mohamed Gamal <mohamed11gamal96@gmail.com>
2019-07-18 14:48:49 +0200

    Add post detail adapter

--- app/src/main/java/trianglz/ui/activities/PostDetailActivity.java
+++ app/src/main/java/trianglz/ui/activities/PostDetailActivity.java
@@ -2,14 +2,22 @@ package trianglz.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.skolera.skolera_android.R;
 
 public class PostDetailActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_detail);
+        bindViews();
+    }
+
+    private void bindViews() {
+        recyclerView = findViewById(R.id.recycler_view);
     }
 }
---
+++ app/src/main/java/trianglz/ui/adapters/PostDetailsAdapter.java
@@ -0,0 +1,30 @@
+package trianglz.ui.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class PostDetailsAdapter extends RecyclerView.Adapter {
+
+    private Context context;
+    public PostDetailsAdapter(Context context) {
+        this.context = context;
+
+    }
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
