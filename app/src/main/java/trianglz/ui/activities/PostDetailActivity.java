5782516d9c4cea897dccc1c60308a789f4cd2b4c
Mohamed Gamal <mohamed11gamal96@gmail.com>
2019-07-18 15:17:00 +0200

    Call post details API

--- app/src/main/java/trianglz/ui/activities/PostDetailActivity.java
+++ app/src/main/java/trianglz/ui/activities/PostDetailActivity.java
@@ -6,18 +6,35 @@ import android.support.v7.widget.RecyclerView;
 
 import com.skolera.skolera_android.R;
 
-public class PostDetailActivity extends AppCompatActivity {
+import trianglz.core.presenters.PostDetailsPresenter;
+import trianglz.core.views.PostDetailsView;
+import trianglz.utils.Constants;
+
+public class PostDetailActivity extends AppCompatActivity implements PostDetailsPresenter {
 
     private RecyclerView recyclerView;
+    private PostDetailsView postDetailsView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_detail);
         bindViews();
+        postDetailsView.getPostDetails(getIntent().getIntExtra(Constants.KEY_COURSE_ID, 0));
     }
 
     private void bindViews() {
         recyclerView = findViewById(R.id.recycler_view);
+        postDetailsView = new PostDetailsView(this, this);
+    }
+
+    @Override
+    public void ongetPostDetailsSuccess() {
+
+    }
+
+    @Override
+    public void ongetPostDetailsFailure() {
+
     }
 }
