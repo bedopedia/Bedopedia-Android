7ffabcf6a5215de8cbf9c7c7326dcaa6b5533eb1
Mohamed Gamal <mohamed11gamal96@gmail.com>
2019-07-30 15:45:31 +0200

    Create course group activity

---
+++ app/src/main/java/trianglz/ui/activities/CourseGroupsActivity.java
@@ -0,0 +1,15 @@
+package trianglz.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.skolera.skolera_android.R;
+
+public class CourseGroupsActivity extends SuperActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_groups);
+    }
+}
