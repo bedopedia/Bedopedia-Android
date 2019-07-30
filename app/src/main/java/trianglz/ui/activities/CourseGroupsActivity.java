d5544e6ef84e825076995bd777d67466e03bdc30
Mohamed Gamal <mohamed11gamal96@gmail.com>
2019-07-30 16:06:23 +0200

    Display course groups

--- app/src/main/java/trianglz/ui/activities/CourseGroupsActivity.java
+++ app/src/main/java/trianglz/ui/activities/CourseGroupsActivity.java
@@ -2,23 +2,69 @@ package trianglz.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.skolera.skolera_android.R;
 
-import trianglz.models.TeacherCourse;
-import trianglz.utils.Constants;
+import java.util.Arrays;
 
-public class CourseGroupsActivity extends SuperActivity {
+import trianglz.components.BottomItemDecoration;
+import trianglz.models.TeacherCourse;
+import trianglz.ui.adapters.TeacherCoursesAdapter;
+import trianglz.utils.Constants;
+import trianglz.utils.Util;
+
+public class CourseGroupsActivity extends SuperActivity implements TeacherCoursesAdapter.TeacherCoursesInterface, View.OnClickListener {
 
     private TeacherCourse teacherCourse;
+    private RecyclerView recyclerView;
+    private TeacherCoursesAdapter teacherCoursesAdapter;
+    private TextView courseNameTextView;
+    private ImageButton backBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_groups);
         getValueFromIntent();
+        bindViews();
+        setListeners();
     }
 
     private void getValueFromIntent() {
-        teacherCourse = TeacherCourse.create(getIntent().getStringExtra(Constants.TEACHER_COURSE));  
+        teacherCourse = TeacherCourse.create(getIntent().getStringExtra(Constants.TEACHER_COURSE));
+    }
+    private void bindViews () {
+        backBtn = findViewById(R.id.btn_back);
+        courseNameTextView = findViewById (R.id.tv_course_name);
+        recyclerView = findViewById(R.id.recycler_view);
+        teacherCoursesAdapter = new TeacherCoursesAdapter(this,this);
+        recyclerView.setAdapter(teacherCoursesAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL,false));
+        recyclerView.addItemDecoration(new BottomItemDecoration((int) Util.convertDpToPixel(6,this),false));
+        courseNameTextView.setText(teacherCourse.getName());
+        teacherCoursesAdapter.addCourseGroups(Arrays.asList(teacherCourse.getCourseGroups()));
+    }
+
+    private void setListeners() {
+        backBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onCourseSelected(TeacherCourse teacherCourse) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_back:
+                onBackPressed();
+                break;
+        }
     }
 }
--- app/src/main/java/trianglz/ui/adapters/TeacherCoursesAdapter.java
+++ app/src/main/java/trianglz/ui/adapters/TeacherCoursesAdapter.java
@@ -16,17 +16,20 @@ import agency.tango.android.avatarview.IImageLoader;
 import agency.tango.android.avatarview.loader.PicassoLoader;
 import agency.tango.android.avatarview.views.AvatarView;
 import trianglz.components.AvatarPlaceholderModified;
+import trianglz.models.CourseGroups;
 import trianglz.models.TeacherCourse;
 
 public class TeacherCoursesAdapter extends RecyclerView.Adapter<TeacherCoursesAdapter.Holder> {
     public Context context;
     public List<TeacherCourse> mDataList;
+    public List<CourseGroups> courseGroupList;
     TeacherCoursesInterface teacherCoursesInterface;
 
 
     public TeacherCoursesAdapter(Context context, TeacherCoursesInterface teacherCoursesInterface) {
         this.context = context;
         this.mDataList = new ArrayList<>();
+        this.courseGroupList = new ArrayList<>();
         this.teacherCoursesInterface = teacherCoursesInterface;
     }
 
@@ -39,26 +42,40 @@ public class TeacherCoursesAdapter extends RecyclerView.Adapter<TeacherCoursesAd
 
     @Override
     public void onBindViewHolder(Holder holder, final int position) {
-        final TeacherCourse teacherCourse = mDataList.get(position);
-        holder.subjectNameTextView.setText(teacherCourse.getName());
-        IImageLoader imageLoader = new PicassoLoader();
-        imageLoader.loadImage(holder.subjectImageView, new AvatarPlaceholderModified(teacherCourse.getName()), "Path");
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                teacherCoursesInterface.onCourseSelected(teacherCourse);
-            }
-        });
+        if (mDataList != null && !mDataList.isEmpty()) {
+            final TeacherCourse teacherCourse = mDataList.get(position);
+            holder.subjectNameTextView.setText(teacherCourse.getName());
+            IImageLoader imageLoader = new PicassoLoader();
+            imageLoader.loadImage(holder.subjectImageView, new AvatarPlaceholderModified(teacherCourse.getName()), "Path");
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    teacherCoursesInterface.onCourseSelected(teacherCourse);
+                }
+            });
+        }
+        if (courseGroupList != null && !courseGroupList.isEmpty()) {
+            final CourseGroups courseGroups = courseGroupList.get(position);
+            holder.subjectNameTextView.setText(courseGroups.getName());
+            IImageLoader imageLoader = new PicassoLoader();
+            imageLoader.loadImage(holder.subjectImageView, new AvatarPlaceholderModified(courseGroups.getName()), "Path");
+        }
     }
 
     @Override
     public int getItemCount() {
-        return mDataList.size();
+        if (!mDataList.isEmpty())return mDataList.size();
+        else return courseGroupList.size();
     }
 
     public void addData(List<TeacherCourse> teacherCourses) {
         this.mDataList.clear();
         if (teacherCourses != null) this.mDataList.addAll(teacherCourses);
+        notifyDataSetChanged();
+    }
+    public void addCourseGroups(List<CourseGroups> courseGroups) {
+        this.courseGroupList.clear();
+        if (courseGroups != null) this.courseGroupList.addAll(courseGroups);
         notifyDataSetChanged();
     }
 
