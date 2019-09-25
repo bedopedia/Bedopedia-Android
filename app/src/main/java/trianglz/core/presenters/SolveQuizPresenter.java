28a1f227d6086be1106f47eee40e719d2cc98769
farahmoniem <farah.ahmed@trianglz.com>
2019-09-25 13:54:05 +0200

    Adjusted signature of on success method

--- app/src/main/java/trianglz/core/presenters/SolveQuizPresenter.java
+++ app/src/main/java/trianglz/core/presenters/SolveQuizPresenter.java
@@ -1,9 +1,11 @@
 package trianglz.core.presenters;
 
+import org.json.JSONObject;
+
 /**
  * Created by Farah A. Moniem on 25/09/2019.
  */
 public interface SolveQuizPresenter {
-    void onGetQuizQuestionsSuccess();
+    void onGetQuizQuestionsSuccess(JSONObject response);
     void onGetQuizQuestionsFailure(String message,int errorCode);
 }
