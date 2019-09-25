21bb7c3ca71233dd7328ea7ce91c3578dd2c0c88
farahmoniem <farah.ahmed@trianglz.com>
2019-09-25 13:53:43 +0200

    Added get quiz questions func in view

--- app/src/main/java/trianglz/core/views/SolveQuizView.java
+++ app/src/main/java/trianglz/core/views/SolveQuizView.java
@@ -1,7 +1,36 @@
 package trianglz.core.views;
 
+import android.content.Context;
+
+import org.json.JSONObject;
+
+import trianglz.core.presenters.SolveQuizPresenter;
+import trianglz.managers.api.ResponseListener;
+import trianglz.managers.api.UserManager;
+
 /**
  * Created by Farah A. Moniem on 25/09/2019.
  */
 public class SolveQuizView {
+    private SolveQuizPresenter solveQuizPresenter;
+    private Context context;
+
+    public SolveQuizView(SolveQuizPresenter solveQuizPresenter, Context context) {
+        this.solveQuizPresenter = solveQuizPresenter;
+        this.context = context;
+    }
+
+    void getQuizQuestions(String url){
+        UserManager.getQuizQuestions(url, new ResponseListener() {
+            @Override
+            public void onSuccess(JSONObject response) {
+                solveQuizPresenter.onGetQuizQuestionsSuccess(response);
+            }
+
+            @Override
+            public void onFailure(String message, int errorCode) {
+                solveQuizPresenter.onGetQuizQuestionsFailure(message, errorCode);
+            }
+        });
+    }
 }
