4cf9943217a387cd33b8fede0ed1c6e0d70bd232
farahmoniem <farah.ahmed@trianglz.com>
2019-09-29 16:20:46 +0200

    Show data in quiz details fragment

--- app/src/main/java/trianglz/ui/fragments/QuizDetailsFragment.java
+++ app/src/main/java/trianglz/ui/fragments/QuizDetailsFragment.java
@@ -12,7 +12,14 @@ import android.widget.TextView;
 
 import com.skolera.skolera_android.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+import trianglz.models.QuizQuestion;
 import trianglz.ui.activities.StudentMainActivity;
+import trianglz.utils.Constants;
 
 /**
  * Created by Farah A. Moniem on 29/09/2019.
@@ -24,6 +31,7 @@ public class QuizDetailsFragment extends Fragment implements View.OnClickListene
     private TextView quizDescriptionTextView, quizNameTextView, publishDateTextView, dueDateTextView, durationTextView,
             totalScoreTextView, bloomsTextView, topicTextView, subtopicTextView, lessonTextView, objectivesTextView,
             courseGroupsTextView, toolbarQuizNameTextView;
+    private QuizQuestion quizQuestion;
 
     @Nullable
     @Override
@@ -42,6 +50,10 @@ public class QuizDetailsFragment extends Fragment implements View.OnClickListene
     }
 
     private void getValueFromIntent() {
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            quizQuestion = QuizQuestion.create(bundle.getString(Constants.KEY_QUIZ_QUESTION));
+        }
     }
 
     private void bindViews() {
@@ -49,7 +61,8 @@ public class QuizDetailsFragment extends Fragment implements View.OnClickListene
         quizDescriptionTextView = rootView.findViewById(R.id.quiz_desc_tv);
         quizNameTextView = rootView.findViewById(R.id.quiz_name_tv);
         publishDateTextView = rootView.findViewById(R.id.publish_date_tv);
-        dueDateTextView = rootView.findViewById(R.id.duration_tv);
+        dueDateTextView = rootView.findViewById(R.id.due_date_tv);
+        durationTextView = rootView.findViewById(R.id.duration_tv);
         totalScoreTextView = rootView.findViewById(R.id.total_score_tv);
         bloomsTextView = rootView.findViewById(R.id.blooms_tv);
         topicTextView = rootView.findViewById(R.id.topic_tv);
@@ -58,10 +71,54 @@ public class QuizDetailsFragment extends Fragment implements View.OnClickListene
         objectivesTextView = rootView.findViewById(R.id.learning_obj_tv);
         courseGroupsTextView = rootView.findViewById(R.id.course_group_tv);
         backButton = rootView.findViewById(R.id.btn_back);
+
+        toolbarQuizNameTextView.setText(quizQuestion.getName());
+        quizNameTextView.setText(quizQuestion.getName());
+        quizDescriptionTextView.setText(quizQuestion.getDescription());
+        publishDateTextView.setText(dateConverter(quizQuestion.getStartDate()));
+        dueDateTextView.setText(dateConverter(quizQuestion.getEndDate()));
+        durationTextView.setText(String.valueOf(quizQuestion.getDuration()));
+        totalScoreTextView.setText(String.valueOf(quizQuestion.getTotalScore()));
+        bloomsTextView.setText(Arrays.toString(quizQuestion.getBlooms()).replaceAll("\\[|\\]", ""));
+        topicTextView.setText(quizQuestion.getUnit().getName());
+        subtopicTextView.setText(quizQuestion.getChapter().getName());
+        lessonTextView.setText(quizQuestion.getLesson().getName());
+        if (!quizQuestion.getObjectives().isEmpty()) {
+            objectivesTextView.setText("");
+            for (int i = 0; i < quizQuestion.getObjectives().size(); i++) {
+                if (i == quizQuestion.getObjectives().size() - 1) {
+                    objectivesTextView.append(quizQuestion.getObjectives().get(i).getName());
+                } else {
+                    objectivesTextView.append(quizQuestion.getObjectives().get(i).getName() + ", ");
+                }
+            }
+        }
+        if (!quizQuestion.getCourseGroups().isEmpty()) {
+            for (int i = 0; i < quizQuestion.getCourseGroups().size(); i++) {
+                if (i == quizQuestion.getCourseGroups().size() - 1) {
+                    courseGroupsTextView.append(quizQuestion.getCourseGroups().get(i).getName());
+                } else {
+                    courseGroupsTextView.append(quizQuestion.getCourseGroups().get(i).getName() + ", ");
+                }
+            }
+        }
     }
 
     private void setListeners() {
         backButton.setOnClickListener(this);
+    }
+
+    private String dateConverter(String dateString) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        Date date = null;
+        try {
+            date = formatter.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        SimpleDateFormat df = new SimpleDateFormat("d MMMM, yyyy - hh:mm a");
+        String formattedDate = df.format(date);
+        return formattedDate;
     }
 
     @Override
