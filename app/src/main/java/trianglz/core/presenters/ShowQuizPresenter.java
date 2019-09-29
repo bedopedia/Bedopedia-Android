671be1e8ca060dab699a6beaf7cc42cd7642ed3e
farahmoniem <farah.ahmed@trianglz.com>
2019-09-29 14:24:22 +0200

    Rename solve quiz presenter

--- app/src/main/java/trianglz/core/presenters/ShowQuizPresenter.java
+++ app/src/main/java/trianglz/core/presenters/ShowQuizPresenter.java
@@ -5,7 +5,7 @@ import trianglz.models.QuizQuestion;
 /**
  * Created by Farah A. Moniem on 25/09/2019.
  */
-public interface SolveQuizPresenter {
+public interface ShowQuizPresenter {
     void onGetQuizQuestionsSuccess(QuizQuestion quizQuestion);
     void onGetQuizQuestionsFailure(String message,int errorCode);
 }
