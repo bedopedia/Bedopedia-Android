2403282b0d477d6e234405b63d5c8a59ecf160e5
farahmoniem <farah.ahmed@trianglz.com>
2019-12-08 18:32:38 +0200

    Add adapter constants

--- app/src/main/java/trianglz/utils/Constants.java
+++ app/src/main/java/trianglz/utils/Constants.java
@@ -255,6 +255,7 @@ public class Constants {
     public static final String KEY_MATCH="match";
     public static final String KEY_QUESTION_ID="question_id";
     public static final String KEY_QUIZ_SUBMISSION_ID="quiz_submission_id";
-
+    public static final int TYPE_ASSIGNMENT = 0;
+    public static final int TYPE_ATTACHMENT = 1;
 }
 
