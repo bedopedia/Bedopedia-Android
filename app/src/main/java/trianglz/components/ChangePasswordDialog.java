495e62a19a7b779c146ea28c2c30668d04d8ace0
Farah Ahmed <farah.ahmed@trianglz.com>
2019-12-15 16:18:07 +0200

    Add password dialog class

---
+++ app/src/main/java/trianglz/components/ChangePasswordDialog.java
@@ -0,0 +1,121 @@
+package trianglz.components;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.skolera.skolera_android.R;
+
+import trianglz.managers.SessionManager;
+import trianglz.utils.Util;
+
+/**
+ * Created by Farah A. Moniem on 15/12/2019.
+ */
+public class ChangePasswordDialog extends Dialog implements DialogInterface.OnShowListener, View.OnClickListener {
+
+    private EditText oldPasswordEditText, newPasswordEditText;
+    private Button updateButton;
+    private ImageButton cancelButton;
+    private TextView oldPasswordShow, newPasswordShow;
+    private Context context;
+    private DialogConfirmationInterface dialogConfirmationInterface;
+
+
+    public ChangePasswordDialog(@NonNull Context context, DialogConfirmationInterface dialogConfirmationInterface) {
+        super(context, R.style.ErrorDialog);
+        View view = getLayoutInflater().inflate(R.layout.change_password_dialog, null);
+        setContentView(view);
+        this.context = context;
+        this.dialogConfirmationInterface = dialogConfirmationInterface;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bindViews();
+        setListeners();
+    }
+
+    private void setListeners() {
+        this.setOnShowListener(this);
+        updateButton.setOnClickListener(this);
+        newPasswordShow.setOnClickListener(this);
+        oldPasswordShow.setOnClickListener(this);
+        cancelButton.setOnClickListener(this);
+    }
+
+    private void bindViews() {
+        getWindow().setDimAmount((float) 0.3);
+        oldPasswordEditText = findViewById(R.id.et_old_password);
+        newPasswordEditText = findViewById(R.id.et_new_password);
+        oldPasswordShow = findViewById(R.id.btn_show_old_password);
+        newPasswordShow = findViewById(R.id.btn_show_new_password);
+        updateButton = findViewById(R.id.update_btn);
+        cancelButton = findViewById(R.id.cancel_btn);
+        oldPasswordShow.setTextColor(context.getResources().getColor(Util.checkUserColor()));
+        newPasswordShow.setTextColor(context.getResources().getColor(Util.checkUserColor()));
+        if (SessionManager.getInstance().getUserType().equals(SessionManager.Actor.STUDENT.toString())) {
+            updateButton.setBackground(context.getResources().getDrawable(R.drawable.curved_salmon_25dp, null));
+        } else if (SessionManager.getInstance().getUserType().equals(SessionManager.Actor.PARENT.toString())) {
+            updateButton.setBackground(context.getResources().getDrawable(R.drawable.curved_turquoise_25, null));
+        } else if (SessionManager.getInstance().getUserType().equals(SessionManager.Actor.TEACHER.toString()) || SessionManager.getInstance().getUserType().equals(SessionManager.Actor.ADMIN.toString()) || SessionManager.getInstance().getUserType().equals(SessionManager.Actor.HOD.toString())) {
+            updateButton.setBackground(context.getResources().getDrawable(R.drawable.curved_cerulean_blue, null));
+        } else {
+            updateButton.setBackground(context.getResources().getDrawable(R.drawable.curved_jade_green_25, null));
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_show_old_password:
+                if (oldPasswordEditText.getTransformationMethod() instanceof PasswordTransformationMethod) {
+                    oldPasswordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    oldPasswordShow.setText(context.getResources().getString(R.string.hide));
+                    oldPasswordShow.setTextColor(context.getResources().getColor(R.color.warm_grey));
+                } else {
+                    oldPasswordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    oldPasswordShow.setText(context.getResources().getString(R.string.show));
+                    oldPasswordShow.setTextColor(context.getResources().getColor(Util.checkUserColor()));
+                }
+                break;
+            case R.id.btn_show_new_password:
+                if (newPasswordEditText.getTransformationMethod() instanceof PasswordTransformationMethod) {
+                    newPasswordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    newPasswordShow.setText(context.getResources().getString(R.string.hide));
+                    newPasswordShow.setTextColor(context.getResources().getColor(R.color.warm_grey));
+                } else {
+                    newPasswordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    newPasswordShow.setText(context.getResources().getString(R.string.show));
+                    newPasswordShow.setTextColor(context.getResources().getColor(Util.checkUserColor()));
+                }
+                break;
+            case R.id.cancel_btn:
+                dismiss();
+                break;
+            case R.id.update_btn:
+                dialogConfirmationInterface.onUpdate();
+                dismiss();
+                break;
+        }
+    }
+
+    @Override
+    public void onShow(DialogInterface dialog) {
+        super.show();
+    }
+
+    public interface DialogConfirmationInterface {
+        void onUpdate();
+    }
+}
