6544b8df959f2a71701a7ffd5c297b5e875310cb
Farah Ahmed <farah.ahmed@trianglz.com>
2019-12-17 18:02:17 +0200

    Refactor set indicator color method

--- app/src/main/java/trianglz/components/LoadingDialog.java
+++ app/src/main/java/trianglz/components/LoadingDialog.java
@@ -2,11 +2,11 @@ package trianglz.components;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StyleRes;
+import android.util.Log;
 import android.view.WindowManager;
 
 import com.skolera.skolera_android.R;
@@ -45,15 +45,16 @@ public class LoadingDialog extends Dialog {
     public void onBackPressed() {
     }
 
-    void setIndicatorColor() {
+    private void setIndicatorColor() {
+        Log.d(SessionManager.getInstance().getUserType() + "  ", "setIndicatorColor: ");
         if (SessionManager.getInstance().getUserType().equals(SessionManager.Actor.STUDENT.toString())) {
-            avLoadingIndicatorView.setIndicatorColor(Color.parseColor("#fd8268"));
+            avLoadingIndicatorView.setIndicatorColor(mContext.getResources().getColor(R.color.salmon));
         } else if (SessionManager.getInstance().getUserType().equals(SessionManager.Actor.PARENT.toString())) {
-            avLoadingIndicatorView.setIndicatorColor(Color.parseColor("#06c4cc"));
-        } else if (SessionManager.getInstance().getUserType().equals(SessionManager.Actor.TEACHER.toString())||SessionManager.getInstance().getUserType().equals(SessionManager.Actor.ADMIN.toString())||SessionManager.getInstance().getUserType().equals(SessionManager.Actor.HOD.toString())) {
-            avLoadingIndicatorView.setIndicatorColor(Color.parseColor("#007ee5"));
-        }else{
-            avLoadingIndicatorView.setIndicatorColor(Color.parseColor("#2ABC4F"));
+            avLoadingIndicatorView.setIndicatorColor(mContext.getResources().getColor(R.color.turquoise_blue_two));
+        } else if (SessionManager.getInstance().getUserType().equals(SessionManager.Actor.TEACHER.toString()) || SessionManager.getInstance().getUserType().equals(SessionManager.Actor.ADMIN.toString()) || SessionManager.getInstance().getUserType().equals(SessionManager.Actor.HOD.toString())) {
+            avLoadingIndicatorView.setIndicatorColor(mContext.getResources().getColor(R.color.cerulean_blue));
+        } else {
+            avLoadingIndicatorView.setIndicatorColor(mContext.getResources().getColor(R.color.colorPrimary));
         }
     }
 }
