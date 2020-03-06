afa4801c534b13529ebf86345c9d9942ccf7dc13
Mohamed Gamal <mohamed11gamal96@gmail.com>
2020-03-06 21:02:03 +0200

    Remove loading dialog from grades

--- app/src/main/java/trianglz/ui/fragments/GradesFragment.java
+++ app/src/main/java/trianglz/ui/fragments/GradesFragment.java
@@ -85,7 +85,6 @@ public class GradesFragment extends Fragment implements GradesAdapter.GradesAdap
 
     private void bindViews() {
         gradesView = new GradesView(getActivity(), this);
-        if (!activity.progress.isShowing()) activity.progress.show();
         gradesView.getGradesCourses(student.actableId);
         activity.toolbarView.setVisibility(View.GONE);
         activity.headerLayout.setVisibility(View.GONE);
