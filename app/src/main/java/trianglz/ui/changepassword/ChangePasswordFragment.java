160da6a05d565a91495591a1e86848297283e2e2
mohamed gamal <mohamed11gamal96@gmail.com>
2020-03-24 15:52:02 +0200

    Set up show and hide

--- app/src/main/java/trianglz/ui/changepassword/ChangePasswordFragment.java
+++ app/src/main/java/trianglz/ui/changepassword/ChangePasswordFragment.java
@@ -1,6 +1,8 @@
 package trianglz.ui.changepassword;
 
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.skolera.skolera_android.R;
 import com.skolera.skolera_android.databinding.ChangePasswordFragmentBinding;
 
 
@@ -28,7 +31,41 @@ public class ChangePasswordFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         viewModel = new ViewModelProvider(this).get(ChangePasswordViewModel.class);
         binding = ChangePasswordFragmentBinding.inflate(inflater, container, false);
-
+        initViewModelObservers();
+        setListeners();
         return binding.getRoot();
+    }
+
+    private void setListeners() {
+        binding.showHideNewPassword.setOnClickListener(v -> onShowHideNewPassword());
+        binding.showHideOldPassword.setOnClickListener(v -> onShowHideOldPassword());
+    }
+
+    private void onShowHideNewPassword() {
+        if (binding.etNewPassword.getTransformationMethod() instanceof PasswordTransformationMethod) {
+            binding.etNewPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            binding.showHideNewPassword.setImageResource(R.drawable.ic_unshow_password);
+        } else {
+            binding.etNewPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            binding.showHideNewPassword.setImageResource(R.drawable.ic_show_password);
+        }
+    }
+    private void onShowHideOldPassword () {
+        if (binding.etOldPassword.getTransformationMethod() instanceof PasswordTransformationMethod) {
+            binding.etOldPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            binding.showHideOldPassword.setImageResource(R.drawable.ic_unshow_password);
+        } else {
+            binding.etOldPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            binding.showHideOldPassword.setImageResource(R.drawable.ic_show_password);
+        }
+    }
+
+    private void initViewModelObservers() {
+        viewModel.oldPassword.observe(getViewLifecycleOwner(), s -> {
+
+        });
+        viewModel.newPassword.observe(getViewLifecycleOwner(), s -> {
+
+        });
     }
 }
--- app/src/main/java/trianglz/ui/changepassword/ChangePasswordViewModel.java
+++ app/src/main/java/trianglz/ui/changepassword/ChangePasswordViewModel.java
@@ -4,5 +4,12 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class ChangePasswordViewModel extends ViewModel {
-    public MutableLiveData<String> oldPassword, newPassword;
+    public MutableLiveData<String> oldPassword;
+    public MutableLiveData<String> newPassword;
+
+    public ChangePasswordViewModel() {
+        super();
+        oldPassword = new MutableLiveData<>();
+        newPassword = new MutableLiveData<>();
+    }
 }
